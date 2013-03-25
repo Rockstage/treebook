3f8ee551d23469c21beb6e26e17849c4ef4b5cea
George Petrov <jokopetrov@gmail.com>
2013-03-25 19:32:44 +0200

    Add status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,9 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
+
+  validates :content, presence: true,
+                     numericality: { greater_than: 2 }
+
+  validates :user_id, presence: true
 end
