1e67ec818f9dc6ce6271bf8958bc3ffc3693ee00
George Petrov <jokopetrov@gmail.com>
2013-03-27 22:00:49 +0200

    Added Gravatar

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sqlite3'
 gem 'devise'
 gem 'simple_form'
 gem 'skrollr-rails'
+gem 'rename'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Treebook::Application.config.secret_token = 'fbc39564f961607179c9d08cda34efdf66f57961028543ce41446a0ab0c031b1e72bf819d3b899f2ec9551722cb8e81bdb62fac16c1fce66f8840e69c17e808a'
+Rockstage::Application.config.secret_token = 'fbc39564f961607179c9d08cda34efdf66f57961028543ce41446a0ab0c031b1e72bf819d3b899f2ec9551722cb8e81bdb62fac16c1fce66f8840e69c17e808a'
