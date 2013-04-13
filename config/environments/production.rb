64424697e14c1938a0b1e2b4b3c134463c8fda56
George Petrov <jokopetrov@gmail.com>
2013-04-13 20:22:40 +0300

    Added ActionMailer config with Gmail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,15 @@ Rockstage::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # Add the fonts path
+  onfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'rockstage.org',
+  :user_name            => 'office@rockstage.org',
+  :password             => 'rockstage3790',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
 
 
