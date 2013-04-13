4464f499d5b4c97e5173dae659f48aec33f125dd
George Petrov <jokopetrov@gmail.com>
2013-04-13 20:15:25 +0300

    Got the app working but with a heroku server error, included Fonts for retesting.

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module Rockstage
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = true
+
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
