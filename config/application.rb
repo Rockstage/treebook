07ad12baa754e7b5000e2fe2b9ad21b89c5b65f0
George Petrov <jokopetrov@gmail.com>
2013-05-16 16:30:12 +0300

    Still issue with rake precompile.

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,9 @@ module Rockstage
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+
+    #Gives an error when false, app error - something went wrong
+    config.assets.initialize_on_precompile = true
 
     # adds app/assets/fonts to the asset path
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
